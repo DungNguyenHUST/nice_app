cb7b469a1eab643c7b80a827b721c2b2e48a7eb9
DungNguyenHUST <vandungbknd@gmail.com>
2021-06-15 13:59:49 +0700

    add follow tag

--- app/controllers/tag_follows_controller.rb
+++ app/controllers/tag_follows_controller.rb
@@ -1,2 +1,53 @@
 class TagFollowsController < ApplicationController
+	before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
+    
+    def index 
+        @tag = Tag.find(params[:tag_id])
+        @tag_follows = @tag.tag_follows
+    end
+
+    def new
+        @tag = Tag.find(params[:tag_id])
+        @tag_follow = TagFollow.new
+    end
+
+    def create
+        @tag = Tag.find(params[:tag_id])
+        @tag_follow = TagFollow.new
+        if !already_followd?
+            @tag_follow = @tag.tag_follows.build(user_id: current_user.id)
+            @tag_follow.save!
+        end
+
+        respond_to do |format|
+            format.html {}
+            format.js
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+    def destroy
+        @tag = Tag.find(params[:tag_id])
+        @tag_follow = @tag.tag_follows.find(params[:id])
+        @tag_follow.destroy
+
+        respond_to do |format|
+            format.html {}
+            format.js
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def already_followd?
+        TagFollow.where(user_id: current_user.id, tag_id: params[:tag_id]).exists?
+    end
 end
