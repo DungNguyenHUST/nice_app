01bb84f170c93320b812bed828fa1d302a572f31
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-02 22:38:52 +0700

    fix

--- db/migrate/20210702035815_drop_tag.rb
+++
@@ -1,7 +0,0 @@
-class DropTag < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :tags, force: :cascade
-    drop_table :taggings, force: :cascade
-    drop_table :tag_follows, force: :cascade
-  end
-end
