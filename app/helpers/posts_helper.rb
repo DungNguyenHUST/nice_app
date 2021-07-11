84a96644fd56cd777e9a2197d87b40eaa223d2fe
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-11 10:37:32 +0700

    fix comment

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -9,12 +9,13 @@ module PostsHelper
     def cal_post_hot_point(post)
         vote_count = post.post_votes.count
         unvote_count = post.post_unvotes.count
+        comment_count = post.post_comments.count
         if post.view_count.present?
             view_count = post.view_count
         else
             view_count = 0
         end
-        point = (vote_count - unvote_count) + view_count
+        point = (vote_count - unvote_count) + view_count + comment_count
         return point
     end
 
