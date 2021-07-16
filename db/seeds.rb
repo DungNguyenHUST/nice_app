a308c75025ea5af971d05c72602d3c5b769abb65
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-16 11:38:22 +0700

    fix infiniti scroll

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |i|
+    Post.create(title: "Post ##{i}", content: "The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup)##{i}", user_id: 1)
+end
