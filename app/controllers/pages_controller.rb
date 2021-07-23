3ad72a1f5d07075134b2250c95421978b2dfaf29
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-23 16:15:19 +0700

    add social algorithem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,109 +1,108 @@
 class PagesController < ApplicationController
-  include PostsHelper
-  include TopicsHelper
-  include Kaminari
-  
-  def home
-    @posts = Post.all.page(params[:page]).per(25)
-    @topics = Topic.all.sort_by{|topic| cal_topic_trend_point(topic)}.reverse.first(25)
+    include PostsHelper
+    include TopicsHelper
+    include Kaminari
+    
+    def home
+        @posts = Post.all.page(params[:page]).per(25)
+        @topics = Topic.all.sort_by{|topic| cal_topic_trend_point(topic)}.reverse.first(10)
 
-    @tab_id = "default"
-    if(params.has_key?(:tab_id))
-        @tab_id = params[:tab_id]
+        @tab_id = "default"
+        if(params.has_key?(:tab_id))
+            @tab_id = params[:tab_id]
+        end
+
+        if "default" == @tab_id || "PostForYou" == @tab_id
+            @buffers = prepare_page_for_you
+            @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+        end
+
+        if "PostTrendID" == @tab_id
+            @buffers = Post.where("created_at >= ?", 1.week.ago.utc)
+            @buffers = @buffers.sort_by{|post| cal_post_trend_point(post)}.reverse
+            @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+        end
+
+        if "PostNewID" == @tab_id
+            @buffers = Post.where("created_at >= ?", 1.week.ago.utc)
+            @posts = @buffers.order('created_at DESC').page(params[:page]).per(25)
+        end
+
+        if "PostTopID" == @tab_id
+            @buffers = Post.all.sort_by{|post| cal_post_top_point(post)}.reverse
+            @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+        end
+
+        if "PostTopWeekID" == @tab_id
+            @buffers = Post.where("created_at >= ?", 1.week.ago.utc)
+            @buffers = @buffers.sort_by{|post| cal_post_top_point(post)}.reverse
+            @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+        end
+
+        if "PostTopMonthID" == @tab_id
+            @buffers = Post.where("created_at >= ?", 1.month.ago.utc)
+            @buffers = @buffers.sort_by{|post| cal_post_top_point(post)}.reverse
+            @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+        end
+
+        if "PostTopYearID" == @tab_id
+            @buffers = Post.where("created_at >= ?", 1.year.ago.utc)
+            @buffers = @buffers.sort_by{|post| cal_post_top_point(post)}.reverse
+            @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+        end
+
+        if "PostPodID" == @tab_id
+            @buffers = Post.where("podcast <> ''")
+            @buffers = @buffers.sort_by{|post| cal_post_trend_point(post)}.reverse
+            @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+        end
+
+        respond_to do |format|
+            format.html {}
+            format.js
+        end
     end
 
-    if "default" == @tab_id || "PostForYou" == @tab_id
-      @buffers = Post.where("created_at >= ?", 1.week.ago.utc)
-      @buffers = @buffers.sort_by{|post| cal_post_hot_point(post)}.reverse
-      @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+    def index
+        @posts = Post.all.page(params[:page]).per(25)
+        @topics = Topic.all.page(params[:page]).per(25)
     end
 
-    if "PostTrendID" == @tab_id
-      @buffers = Post.where("created_at >= ?", 1.week.ago.utc)
-      @buffers = @buffers.sort_by{|post| cal_post_trend_point(post)}.reverse
-      @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+    def search
+        if(params.has_key?(:search))
+        @search = params[:search]
+        @posts_search = Post.search(@search).page(params[:page]).per(25)
+        @users_search = User.search(@search).page(params[:page]).per(25)
+        @topics_search = Topic.search(@search).page(params[:page]).per(25)
+        end
+
+        @tab_id = "default"
+        if(params.has_key?(:tab_id))
+            @tab_id = params[:tab_id]
+        end
     end
 
-    if "PostNewID" == @tab_id
-      @buffers = Post.where("created_at >= ?", 1.week.ago.utc)
-      @posts = @buffers.order('created_at DESC').page(params[:page]).per(25)
+    # GET /pages/1 or /pages/1.json
+    def show
     end
 
-    if "PostTopID" == @tab_id
-      @buffers = Post.all.sort_by{|post| cal_post_top_point(post)}.reverse
-      @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+    # GET /pages/new
+    def new
     end
 
-    if "PostTopWeekID" == @tab_id
-      @buffers = Post.where("created_at >= ?", 1.week.ago.utc)
-      @buffers = @buffers.sort_by{|post| cal_post_top_point(post)}.reverse
-      @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+    # GET /pages/1/edit
+    def edit
     end
 
-    if "PostTopMonthID" == @tab_id
-      @buffers = Post.where("created_at >= ?", 1.month.ago.utc)
-      @buffers = @buffers.sort_by{|post| cal_post_top_point(post)}.reverse
-      @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+    # POST /pages or /pages.json
+    def create
     end
 
-    if "PostTopYearID" == @tab_id
-      @buffers = Post.where("created_at >= ?", 1.year.ago.utc)
-      @buffers = @buffers.sort_by{|post| cal_post_top_point(post)}.reverse
-      @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+    # PATCH/PUT /pages/1 or /pages/1.json
+    def update
     end
 
-    if "PostPodID" == @tab_id
-      @buffers = Post.where("podcast <> ''")
-      @buffers = @buffers.sort_by{|post| cal_post_trend_point(post)}.reverse
-      @posts = Kaminari.paginate_array(@buffers).page(params[:page]).per(25)
+    # DELETE /pages/1 or /pages/1.json
+    def destroy
     end
-
-    respond_to do |format|
-      format.html {}
-      format.js
-    end
-  end
-
-  def index
-    @posts = Post.all.page(params[:page]).per(25)
-    @topics = Topic.all.page(params[:page]).per(25)
-  end
-
-  def search
-    if(params.has_key?(:search))
-      @search = params[:search]
-      @posts_search = Post.search(@search).page(params[:page]).per(25)
-      @users_search = User.search(@search).page(params[:page]).per(25)
-      @topics_search = Topic.search(@search).page(params[:page]).per(25)
-    end
-
-    @tab_id = "default"
-    if(params.has_key?(:tab_id))
-        @tab_id = params[:tab_id]
-    end
-  end
-
-  # GET /pages/1 or /pages/1.json
-  def show
-  end
-
-  # GET /pages/new
-  def new
-  end
-
-  # GET /pages/1/edit
-  def edit
-  end
-
-  # POST /pages or /pages.json
-  def create
-  end
-
-  # PATCH/PUT /pages/1 or /pages/1.json
-  def update
-  end
-
-  # DELETE /pages/1 or /pages/1.json
-  def destroy
-  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,17 @@
 module PostsHelper
 include PostVotesHelper
+include UsersHelper
+    def cal_post_for_you_point(post)
+        if(post.post_comments.count > 0)
+            post_commments = post.post_comments.sort_by{|post_comment| post_comment.created_at}.reverse
+            lastest_comment = post_commments.first
+            comment_point = Time.now - lastest_comment.created_at
+        else
+            comment_point = Time.now - post.created_at
+        end
+        return comment_point
+    end
+
     def cal_post_top_point(post)
         vote_count = count_post_vote(post)
         return vote_count
@@ -26,5 +38,41 @@ include PostVotesHelper
         end
         point = view_count + comment_count
         return point
+    end
+
+    def prepare_page_for_you
+        # For logged in user
+        processing_datas = []
+        if user_signed_in?
+            # Find post by followed user
+            followed_users = current_user.followees
+            followed_users.each do |followed_user|
+                post_by_followed_users = find_owner_post_for_user(followed_user)
+                post_by_followed_users.each do |post_by_followed_user|
+                    processing_datas.push(post_by_followed_user)
+                end
+            end
+
+            # Find post by followed topic
+            followed_topics = find_owner_topic_follow_for_user(current_user)
+            followed_topics.each do |followed_topic|
+                post_by_followed_topics = followed_topic.posts
+                post_by_followed_topics.each do |post_by_followed_topic|
+                    processing_datas.push(post_by_followed_topic)
+                end
+            end
+
+            # Remove duplicate data 
+            processing_datas.uniq{ |post| [post.id]}
+
+            # Sort data by lastest comment
+            processing_datas = processing_datas.sort_by{|post| cal_post_for_you_point(post)}
+        else
+            # For 
+            processing_datas = Post.all
+            processing_datas = processing_datas.sort_by{|post| cal_post_for_you_point(post)}
+        end
+
+        return processing_datas
     end
 end
