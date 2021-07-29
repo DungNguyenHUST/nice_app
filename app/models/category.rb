21b068433384b76effb99241528878b14dfc4fa0
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-29 15:09:28 +0700

    convert slug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,17 @@
 class Category < ApplicationRecord
     extend FriendlyId
-    friendly_id :name, use: :slugged
+    def convert_slug
+        slug = name.downcase.to_s
+        slug.gsub! /[àáạãảâậấẫầẩăặắằẵẳ]/, "a"
+        slug.gsub! /[đ]/, "d"
+        slug.gsub! /[èéẹẽẻêềếệễể]/, "e"
+        slug.gsub! /[óòọõỏôốồộỗổơớợỡờở]/, "o"
+        slug.gsub! /[úùụũủưứựừữử]/, "u"
+        slug.gsub! /[íịìĩỉ]/, "i"
+        slug.gsub! /[ýỵỹỳỷ]/, "y"
+        return slug
+    end
+    friendly_id :convert_slug, use: :slugged
 
     has_many :topics
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,18 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  def convert_slug
+    slug = name.downcase.to_s
+    slug.gsub! /[àáạãảâậấẫầẩăặắằẵẳ]/, "a"
+    slug.gsub! /[đ]/, "d"
+    slug.gsub! /[èéẹẽẻêềếệễể]/, "e"
+    slug.gsub! /[óòọõỏôốồộỗổơớợỡờở]/, "o"
+    slug.gsub! /[úùụũủưứựừữử]/, "u"
+    slug.gsub! /[íịìĩỉ]/, "i"
+    slug.gsub! /[ýỵỹỳỷ]/, "y"
+    return slug
+  end
+  friendly_id :convert_slug, use: :slugged
 
   mount_uploader :avatar, ImageUploader
   mount_uploader :cover_image, ImageUploader
