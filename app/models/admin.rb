d836a28aecdf0af92c2224391bca2f46f729a2cd
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-30 17:43:17 +0700

    lock admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :trackable, :timeoutable, :lockable, :registerable
+  devise :database_authenticatable, :trackable, :timeoutable, :lockable
 end
