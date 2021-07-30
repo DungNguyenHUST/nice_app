17065ed5dbf55a2de598d338fb92232f3a68878d
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-30 17:35:01 +0700

    fix login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,62 +1,66 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+    before_action :save_my_previous_url, only: [:new]
+    # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+    # GET /resource/sign_in
+    # def new
+    #   super
+    # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    # POST /resource/sign_in
+    # def create
+    #   super
+    # end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    # DELETE /resource/sign_out
+    # def destroy
+    #   super
+    # end
 
-  # protected
+    # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_in_params
+    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    # end
 
-  def after_sign_in_path_for(resource_or_scope)
-    current_path = request.referrer
+    # The path used after sign in.
+    def after_sign_in_path_for(resource_or_scope)
+        case session[:my_previous_url]
+        when new_user_session_path
+        when user_session_path
+        when destroy_user_session_path
+        when user_facebook_omniauth_authorize_path
+        when user_facebook_omniauth_callback_path
+        when user_google_oauth2_omniauth_authorize_path
+        when user_google_oauth2_omniauth_callback_path
+        when new_user_password_path
+        when edit_user_password_path
+        when user_password_path
+        when cancel_user_registration_path
+        when new_user_registration_path
+        when edit_user_registration_path
+        when user_registration_path
+        when users_path
+        when new_user_path
+        when edit_user_path(current_user)
+        when user_path(current_user)
+            session[:my_previous_url] = root_path
+        else
+        end
 
-    case current_path
-    when new_user_session_path
-    when user_session_path
-    when destroy_user_session_path
-    when user_facebook_omniauth_authorize_path
-    when user_facebook_omniauth_callback_path
-    when user_google_oauth2_omniauth_authorize_path
-    when user_google_oauth2_omniauth_callback_path
-    when new_user_password_path
-    when edit_user_password_path
-    when user_password_path
-    when cancel_user_registration_path
-    when new_user_registration_path
-    when edit_user_registration_path
-    when user_registration_path
-    when users_path
-    when new_user_path
-    when edit_user_path(current_user)
-    when user_path(current_user)
-      current_path = root_path
-    else
+        # Show wellcome path
+        if current_user.sign_in_count == 0
+            session[:my_previous_url] = user_wellcome_path
+        end
+        current_user.increment!(:sign_in_count)
+
+        stored_location_for(resource_or_scope) || session[:my_previous_url]
     end
 
-    # Show wellcome path
-    if current_user.sign_in_count == 0
-      current_path = user_wellcome_path
+    def save_my_previous_url
+        session[:my_previous_url] = URI(request.referer || '').path
     end
-    current_user.increment!(:sign_in_count)
-
-    stored_location_for(resource_or_scope) || current_path
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -78,21 +78,4 @@ module ApplicationHelper
         uri = url.split("/")[2].sub(/^www./,'').upcase
         return uri.to_s
     end
-
-    # Login anywhere
-    def resource_name
-        :user
-    end
-    
-    def resource
-        @resource ||= User.new
-    end
-
-    def  resource_class 
-        User 
-    end
-    
-    def devise_mapping
-        @devise_mapping ||= Devise.mappings[:user]
-    end
 end
